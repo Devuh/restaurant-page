fdedb2d0ef40a489058954e2ecccd63bdebf1060
Devuh <david.walter.reichert@gmail.com>
2025-05-02 16:18:19 -0500

    Create contact page

--- src/about.js
+++ src/about.js
@@ -1,3 +1,27 @@
 export function about() {
-    
+    const content = document.querySelector("#content");
+    while (content.firstChild) {
+        content.removeChild(content.lastChild);
+    }
+    const title = document.createElement("h2");
+    title.textContent = "Contact Us!";
+    content.appendChild(title);
+    const labelName = document.createElement("label");
+    labelName.textContent = "Name: ";
+    const inputName = document.createElement("input");
+    content.appendChild(labelName);
+    content.appendChild(document.createElement("br"));
+    content.appendChild(inputName);
+    const labelMessage = document.createElement("label");
+    labelMessage.textContent = "Message: ";
+    const inputMessage = document.createElement("textarea");
+    content.appendChild(document.createElement("br"));
+    content.appendChild(labelMessage);
+    content.appendChild(document.createElement("br"));
+    content.appendChild(inputMessage);
+    const submit = document.createElement("button");
+    submit.textContent = "Send";
+    content.appendChild(document.createElement("br"));
+    submit.id = "submit";
+    content.appendChild(submit);
 }
